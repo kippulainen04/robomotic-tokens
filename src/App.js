ad10a776ea07ab6f02a55e3d3420fc5ca6aeb0a5
Nhan Phan <kippulainenko@gmail.com>
2022-01-22 22:53:54 +0200

    Adding giles for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';  
+
+import { CardList } from './components/card-list/card-list.component';
+
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// to get whatever functionality that this React componenet that was built into the libary by other developers React.Component
+class App extends Component {
+  
+  constructor() {
+    super();
+// super(), so it gives us access to this.state by calling React.Component's constructor ()
+    this.state = {
+      robots:  [],
+      searchField: ''
+    };
+  }
+// fetch to make an API request to the URL, return a promise, then we turn it into a json
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()) 
+    .then(users => this.setState({robots: users}));
+  } 
+
+// {} is a JS expression
+  render() {
+    const { robots, searchField} = this.state;
+    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+      <div className="App">
+        <h1>Robomotic Token</h1>
+        <SearchBox 
+          placeholder='search robots' 
+          handleChange={e => this.setState({ searchField: e.target.value})}
+        /> 
+        <CardList robots={filteredRobots}/>
+        
+      </div>
+    );
+  }  
 }
 
 export default App;
