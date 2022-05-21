8d770b11b0e82020e2533da69498f63d3352988d
Nhan Phan <84224787+kippulainen04@users.noreply.github.com>
2022-05-22 00:33:31 +0300

    Update card.component.jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,7 +6,7 @@ export const Card = props => (
 <div className='card-container'>
     <img 
      alt="robot" 
-     src={`https://robohash.org/${props.robot.id}?set=set1&size=180x180`}
+     src={`https://robohash.org/${props.robot.id}?size=180x180`}
      />
     <h2>{props.robot.name}</h2>
     <p>{props.robot.email}</p>
