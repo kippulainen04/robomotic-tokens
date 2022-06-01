557d5a40f329dede260e3463809855f203bb953c
Nhan Phan <kippulainenko@gmail.com>
2022-06-01 22:28:31 +0300

    play around with PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -16,3 +18,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
+
